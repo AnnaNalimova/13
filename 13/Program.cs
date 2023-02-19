239eff2748b46b53978302d3639e1ca96b6021e6
Anna Nalimova <avnalimova@mail.ru>
2023-02-19 23:16:18 +0300

    13

---
+++ 13/Program.cs
@@ -0,0 +1,18 @@
+﻿int num =new Random().Next(25,100000);
+Console.WriteLine(num);
+if(num<100)
+Console.WriteLine(" Третьего числа нет");
+if(num<1000 && num>=100) 
+   num=num%10;
+Console.WriteLine($"Третье число -> {num}");
+if(num>=1000)
+{ while(num>999)
+  {
+   num=num/10;
+  };
+  num=num%10;
+     Console.WriteLine($"Третье число -> {num} ");
+     }
+
+
+   
